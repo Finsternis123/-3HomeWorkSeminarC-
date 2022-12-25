dd4c6eca6d89d64428cd99b85e3775b6f62eb26b
Vladislav <zomba4o4ek100@gmail.com>
2022-12-25 15:03:02 +0300

    First Task Complete

---
+++ Task№1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Enter number: ");
+string number = Console.ReadLine();
+
+if (number[0] == number[4] && number[1] == number[3])
+{
+    Console.WriteLine($"Является палиндромом");
+}   
+else
+{
+    Console.WriteLine($"Не является палиндромом");
+}
+
