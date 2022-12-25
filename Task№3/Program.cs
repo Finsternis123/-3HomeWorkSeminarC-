96ca01d0746b9e7b140f3c50af80d62480f69515
Vladislav <zomba4o4ek100@gmail.com>
2022-12-25 15:17:58 +0300

    Third Task Complete

---
+++ Task№3/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.Write("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+ int count = 1;
+ while (count <= num)
+ {
+     Console.Write(Math.Pow(count, 3)+" ");
+     count++;
+ }
